bd0f95b5dec372a4ade30399e20890528860d1c2
woubie <abraham.woubie@gmail.com>
2018-12-07 16:55:51 +0200

    Updating

--- Agent.py
+++ Agent.py
@@ -49,8 +49,10 @@ class Agent:
     def display_greedy_policy(self):
         # greedy policy = argmax[a'] Q[s,a']
         greedy_policy = np.zeros((self.state_dim[0], self.state_dim[1]), dtype=int)
+        print(self.Q)
         for x in range(self.state_dim[0]):
             for y in range(self.state_dim[1]):
+                #print(self.Q[y, x, :])
                 greedy_policy[y, x] = np.argmax(self.Q[y, x, :])
         print("\nGreedy policy(y, x):")
         print(greedy_policy)
--- Environment.py
+++ Environment.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 
 from Agent import Agent
 
+#from Extract import Extract_Samples
+
+from scipy.spatial import  distance
 
 
 """
@@ -24,8 +27,8 @@ from Agent import Agent
 
 """
 
-nRow=3
-nCol=3
+nRow=8
+nCol=8
 
 class Environment:
     
@@ -39,10 +42,11 @@ class Environment:
         self.action_dict = {"up": 0, "right": 1, "down": 2, "left": 3}
         self.action_coords = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # translations
         # Define rewards table
-        self.R = self._build_rewards()  # R(s,a) agent rewards
+        #self.R = self._build_rewards()  # R(s,a) agent rewards
         # Check action space consistency
         if len(self.action_dict.keys()) != len(self.action_coords):
             exit("err: inconsistent actions given")
+
 
     def reset(self):
         # Reset agent state to top-left grid corner
@@ -51,16 +55,30 @@ class Environment:
         self.state = (start_row, start_col)
         return self.state
 
+
+
     def step(self, action):
         # Evolve agent state
         state_next = (self.state[0] + self.action_coords[action][0],
                       self.state[1] + self.action_coords[action][1])
+
+        samples_state_next=Extract_Samples(state_next[0],state_next[1])
+        samples_goal_state = Extract_Samples(nRow - 1, nCol - 1)
+
+        #print("Extracted Samples of row {} col {} is {}".format(state_next[0],state_next[1],samples_state_next))
+
         # Collect reward
-        reward = self.R[self.state + (action,)] #  state(0,0) and action (1,)=>(0,0,1)
+        #reward = self.R[self.state + (action,)] #  state(0,0) and action (1,)=>(0,0,1)
+        if(distance.euclidean(samples_state_next,samples_goal_state)==0):
+            reward=1
+            done=True
+        else:
+            reward=0
+            done=False
 
         # Terminate if we reach bottom-r
         # right grid corner
-        done = (state_next[0] == self.nRow - 1) and (state_next[1] == self.nCol - 1)
+        #done = (state_next[0] == self.nRow - 1) and (state_next[1] == self.nCol - 1)
         # Update state
         self.state = state_next
         return state_next, reward, done
@@ -83,11 +101,26 @@ class Environment:
         #print("actions allowed",actions_allowed)
         return actions_allowed
 
-    def _build_rewards(self):
-        # Define agent rewards R[s,a]
-        reward_goal = 1  # reward for arriving at terminal state (bottom-right corner)
-        reward_no_goal = 0  # penalty for not reaching terminal state
-        R = reward_no_goal * np.ones(self.state_dim + self.action_dim, dtype=float)  # R[s,a]
-        R[self.nRow - 2, self.nCol - 1, self.action_dict["down"]] = reward_goal  # arrive from above
-        R[self.nRow - 1, self.nCol - 2, self.action_dict["right"]] = reward_goal  # arrive from the left
-        return R
+    # def _build_rewards(self):
+    #     # Define agent rewards R[s,a]
+    #     reward_goal = 1  # reward for arriving at terminal state (bottom-right corner)
+    #     reward_no_goal = 0  # penalty for not reaching terminal state
+    #     R = reward_no_goal * np.ones(self.state_dim + self.action_dim, dtype=float)  # R[s,a]
+    #     R[self.nRow - 2, self.nCol - 1, self.action_dict["down"]] = reward_goal  # arrive from above
+    #     R[self.nRow - 1, self.nCol - 2, self.action_dict["right"]] = reward_goal  # arrive from the left
+    #     return R
+
+def Extract_Samples(row, col):
+
+    fs = 100  # sample rate
+    f = 2  # the frequency of the signal
+
+    x = np.arange(fs)  # the points on the x axis for plotting
+
+    # compute the value (amplitude) of the sin wave at the for each sample
+    # if letter in b'G':
+    if(row==nRow and col==nCol):
+        samples = [100 + row + col + np.sin(2 * np.pi * f * (i / fs)) for i in x]
+    else:
+        samples = [row + col + np.sin(2 * np.pi * f * (i / fs)) for i in x]
+    return samples
